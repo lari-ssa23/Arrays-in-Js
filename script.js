8e8a3762538eda3142cfda040604a155174d8727
lari-ssa23 <larissa.isabel@unesp.br>
2022-01-26 19:56:03 -0300

    arrays

---
+++ script.js
@@ -0,0 +1,22 @@
+//exercicio1
+let aprendizados = ["programa css", "frontend", "marcaçao html", "Sociemocional", "ingles"]
+console.log(aprendizados)
+//exercicio2
+aprendizados.unshift("java script")
+console.log(aprendizados)
+//exercicio3
+ aprendizados.pop("trabalho conjunto")
+ console.log(aprendizados)
+ //exercicio4
+ aprendizados.push("ingles","trabalho conjunto")
+ console.log(aprendizados)
+ //exercicio5
+ aprendizados.shift(0)
+  console.log(aprendizados)
+  //exercicio6
+ let numbers = [7,5,6,3,8,9,2,1,4]
+ numbers.sort(function(a, b){
+     return (a-b)
+ })
+ console.log(numbers)
+
